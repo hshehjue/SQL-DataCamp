4502576f3bc3d06fee6b6524622afc3c252227ed
Alee_miel <63544800+hshehjue@users.noreply.github.com>
2020-04-15 18:08:30 +0900

    sixth

---
+++ Relational_Databases/06. Delete tables with DROP TABLE.sql
@@ -0,0 +1,12 @@
+/* Obviously, the university_professors table is now no longer needed and can safely be deleted.
+
+For table deletion, you can use the simple command:
+
+DROP TABLE table_name; */
+
+
+/*INSTRUCTION 
+Delete the university_professors table. */
+
+-- Delete the university_professors table
+DROP TABLE university_professors;
