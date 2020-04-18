94ead817c8688986b0ccfb127b63a25735dae999
Alee_miel <63544800+hshehjue@users.noreply.github.com>
2020-04-18 21:07:21 +0900

    twentith

--- Relational_Databases/20. REFERENCE a table with a FOREIGN KEY.sql
+++ Relational_Databases/20. REFERENCE a table with a FOREIGN KEY.sql
@@ -16,3 +16,13 @@ Rename the university_shortname column to university_id in professors. */
 -- Rename the university_shortname column
 ALTER TABLE professors
 RENAME COLUMN university_shortname TO university_id;
+
+
+/* INSTRUCTION (2/2)
+
+* Add a foreign key on university_id column in professors that references the id column in universities.
+* Name this foreign key professors_fkey.*/
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
